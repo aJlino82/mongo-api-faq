3d0f4bc271b779e50bcb7584701d281595cfc32b
Adailton Junior <adailton_jr@hotmail.com>
2022-10-05 16:58:49 -0300

    adicionando express e ejs para integração front back

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
-const express = require('express')
+const express = require('express');
+const request = require('request-promise-native');
+
 const app = express()
 
-app.get('/produtos', function (req, res) {
-    res.send('Olá mundo!')
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.get('/produtos', async function (req, res) {
+    const result = await request.get('http://localhost:3000/api/v1/products');
+    res.render('products');
 })
 
 app.listen(3001, function () {
